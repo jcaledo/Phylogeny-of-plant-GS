21f525a293427b3d23dd2c9c6334841f62ae2aef
JC Aledo <caledo@uma.es>
2024-07-23 13:36:59 +0200

    Figure1.R and Representatives.Rda

---
+++ Figure1_Seed_Plants_4_Clusters.R
@@ -0,0 +1,19 @@
+## -------------------------------------------------- ##
+## ---     Figure 1 (Seed Plants 4 Clusters)      --- ##
+## -------------------------------------------------- ##
+# Make sure you have installed the following R packages:
+
+# orthGS
+# EnvNJ
+# muscle
+
+# Uncomment the required lines below:
+
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# BiocManager::install("muscle")
+# install.packages("orthGS")
+# install.packages("EnvNJ")
+
+library(orthGS)
+
